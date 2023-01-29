846cd73ac7ca6e2d749bdef9acd722c91bf09ac5
Leon San José Larsson <leonlarsson8@gmail.com>
2023-01-29 16:21:23 +0100

    data: change to top 20 users and fix layout

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -45,7 +45,7 @@ export default () => {
           </div>
 
           <div className="row">
-            <div className="col mb-3">
+            <div className="col-lg mb-3">
               <h4>Stats sent per game</h4>
               <ul className="list-group">
                 {Array.from(new Set(outputs.map(x => x.game))).map((game, index) => <li className="list-group-item" key={index}>{game}: <strong>{outputs.filter(x => x.game === game).length.toLocaleString("en-US")}</strong> ({(outputs.filter(x => x.game === game).length / outputs.length * 100).toFixed(1)}%)</li>)}
@@ -65,7 +65,7 @@ export default () => {
               </ul>
             </div>
 
-            <div className="col mb-3">
+            <div className="col-lg mb-3">
               <h4>Stats sent per segment</h4>
               <ul className="list-group">
                 {Array.from(new Set(outputs.map(x => x.segment))).map((segment, index) => <li className="list-group-item" key={index}>{segment}: <strong>{outputs.filter(x => x.segment === segment).length.toLocaleString("en-US")}</strong> ({(outputs.filter(x => x.segment === segment).length / outputs.length * 100).toFixed(1)}%)</li>)}
@@ -85,7 +85,7 @@ export default () => {
               </ul>
             </div>
 
-            <div className="col mb-3">
+            <div className="col-lg mb-3">
               <h4>Stats sent per language</h4>
               <ul className="list-group">
                 {Array.from(new Set(outputs.map(x => x.language))).map((language, index) => <li className="list-group-item" key={index}>{language}: <strong>{outputs.filter(x => x.language === language).length.toLocaleString("en-US")}</strong> ({(outputs.filter(x => x.language === language).length / outputs.length * 100).toFixed(1)}%)</li>)}
@@ -105,13 +105,12 @@ export default () => {
               </ul>
             </div>
 
-            <div className="col">
-              <h4>Top 10 users</h4>
+            <div className="col-lg">
+              <h4>Top users</h4>
               <ol className="list-group list-group-numbered">
-                {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((x, index) => <li className="list-group-item" key={index}>User was sent <strong>{users[x].total_stats_sent.toLocaleString("en-US")}</strong> stats</li>)}
+                {[...Array(20).keys()].map(x => <li className="list-group-item" key={x}>User was sent <strong>{users[x].total_stats_sent.toLocaleString("en-US")}</strong> stats</li>)}
               </ol>
             </div>
-
           </div>
 
           <hr className="border border-primary border-2 opacity-75 rounded" />
@@ -125,7 +124,7 @@ export default () => {
           </div>
 
           <div className="row">
-            <div className="col mb-3">
+            <div className="col-lg mb-3">
               <h4>Stats sent per game</h4>
               <ul className="list-group">
                 {Object.entries(baseStats.totalStatsSent.games).map((game, index) => <li className="list-group-item" key={index}>{game[0]}: <strong>{game[1].toLocaleString("en-US")}</strong> ({(game[1] / baseStats.totalStatsSent.total * 100).toFixed(1)}%)</li>)}
@@ -145,7 +144,7 @@ export default () => {
               </ul>
             </div>
 
-            <div className="col">
+            <div className="col-lg">
               <h4>Stats sent per language</h4>
               <ul className="list-group">
                 {Object.entries(baseStats.totalStatsSent.languages).map((language, index) => <li className="list-group-item" key={index}>{language[0]}: <strong>{language[1].toLocaleString("en-US")}</strong> ({(language[1] / baseStats.totalStatsSent.total * 100).toFixed(1)}%)</li>)}
