ea9a7e3544714e3d8030be9b004fec7bc49563e7
Leon San José Larsson <leonlarsson8@gmail.com>
2023-01-30 19:17:50 +0100

    data: brain no worky

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -177,7 +177,7 @@ export default () => {
 
           <h3>Last stats sent</h3>
           <div className="mb-3">
-            <button className="btn btn-primary mb-2" onClick={() => setShowAllOutputs(!showAllOutputs)}>{showAllOutputs ? "Back to top 20" : "Show all outputs (SLOW)"}</button>
+            <button className="btn btn-primary mb-2" onClick={() => setShowAllOutputs(!showAllOutputs)}>{showAllOutputs ? "Back to last 20" : "Show all outputs (SLOW)"}</button>
             <ul className="list-group list-group-numbered">
               {outputs.sort((a, b) => b.date - a.date).slice(0, showAllOutputs ? -1 : 20).map((output, index) => <li key={index} className="list-group-item"><strong>{output.game} {output.segment}</strong> - <strong>{output.language}</strong> // {new Date(output.date).toUTCString()} ({humanizeDuration(output.date - new Date(), { round: true })} ago)</li>)}
             </ul>
