3b6c43e26e94eeb8a4e02a8b5fbab9bfee04049f
Leon San José Larsson <leonlarsson8@gmail.com>
2023-02-26 16:08:57 +0100

    use newer react-router structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./components/Home";
-import Privacy from "./components/Privacy";
-import Tos from "./components/Tos";
-import DevBrowseUsers from "./components/DevBrowseUsers";
-import DevBrowseOutputs from "./components/DevBrowseOutputs";
-import Data from "./components/Data";
-import NotFound from "./components/NotFound";
+import { Outlet } from "react-router-dom";
+import Nav from "./components/Nav";
 
 export default () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Header />}>
-                    <Route index element={<Home />} />
-                    <Route path="privacy" element={<Privacy />} />
-                    <Route path="tos" element={<Tos />} />
-                    <Route path="dev-browse-users" element={<DevBrowseUsers />} />
-                    <Route path="dev-browse-outputs" element={<DevBrowseOutputs />} />
-                    <Route path="data" element={<Data />} />
-                    <Route path="*" element={<NotFound />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        <>
+            <Nav />
+            <Outlet />
+        </>
     );
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import App from "./App";
+import Home from "./components/Home";
+import Privacy from "./components/Privacy";
+import Tos from "./components/Tos";
+import DevBrowseUsers from "./components/DevBrowseUsers";
+import DevBrowseOutputs from "./components/DevBrowseOutputs";
+import Data from "./components/Data";
+import NotFound from "./components/NotFound";
+
 const queryClient = new QueryClient();
 
-root.render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "privacy", element: <Privacy /> },
+      { path: "tos", element: <Tos /> },
+      { path: "dev-browse-users", element: <DevBrowseUsers /> },
+      { path: "dev-browse-outputs", element: <DevBrowseOutputs /> },
+      { path: "data", element: <Data /> },
+      { path: "*", element: <NotFound /> }
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>
 );
