0ca06e62f843c905a4718c7440c2accd9b8096a7
Leon San José Larsson <leonlarsson8@gmail.com>
2023-04-14 17:00:13 +0200

    do the reverting

--- app/components/Data/LastSentList.tsx
+++ app/components/Data/LastSentList.tsx
@@ -1,47 +1,35 @@
 "use client";
 
+import { useState, useTransition } from "react";
 import humanizeDuration from "humanize-duration";
-import { Table } from "antd";
-import type { ColumnsType } from "antd/es/table";
 import type { Output } from "@/types";
 
 const LastSentList = ({ outputs }: { outputs: Output[] }) => {
 
-    const columns: ColumnsType<Output> = [
-        {
-            title: "Game",
-            dataIndex: "game",
-            filters: [...new Set(outputs.map(output => output.game))].map(game => ({ text: game, value: game })),
-            onFilter: (value, record: Output) => record.game === value
-        },
-        {
-            title: "Segment",
-            dataIndex: "segment",
-            filters: [...new Set(outputs.map(output => output.segment))].map(segment => ({ text: segment, value: segment })),
-            onFilter: (value, record: Output) => record.segment === value
-        },
-        {
-            title: "Language",
-            dataIndex: "language",
-            filters: [...new Set(outputs.map(output => output.language))].map(language => ({ text: language, value: language })),
-            onFilter: (value, record: Output) => record.language === value
-        },
-        {
-            title: "Date",
-            dataIndex: "date",
-            render: (date: number) => <span suppressHydrationWarning>{new Date(date).toUTCString()} ({humanizeDuration(date - new Date().getTime(), { round: true })} ago)</span>
-        }
-    ];
+    const [showAllOutputs, setShowAllOutputs] = useState(false);
+    const [isPending, startTransition] = useTransition();
 
     return (
-        <Table
-            className="mb-5"
-            columns={columns}
-            dataSource={outputs.sort((a, b) => b.date - a.date).map(output => ({ ...output, key: output.date }))}
-            bordered
-            scroll={{ x: true }}
-            pagination={{ hideOnSinglePage: true }}
-        />
+        <div className="mb-5">
+            <button
+                className="btn btn-primary mb-2"
+                disabled={isPending}
+                onClick={() => startTransition(() => setShowAllOutputs(!showAllOutputs))}
+            >
+                {showAllOutputs ? "Show last 20" : `Show all ${outputs.length.toLocaleString("en-US")} (might freeze your browser)`} {isPending && <i className="fa-solid fa-spinner fa-spin" />}
+            </button>
+            <ul className="list-group list-group-numbered">
+                {outputs
+                    .sort((a, b) => b.date - a.date)
+                    .slice(0, showAllOutputs ? -1 : 20)
+                    .map(output => (
+                        <li key={output.date} className="list-group-item">
+                            <strong>{output.game} {output.segment}</strong> - <strong>{output.language}</strong> // <span suppressHydrationWarning title={new Date(output.date).toUTCString()}>{humanizeDuration(output.date - new Date().getTime(), { round: true })} ago</span>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
     );
 };
 
