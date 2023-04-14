86ce74c61e3fd6b854a1e5f2d1a70de9ff5d4f1d
Leon San José Larsson <leonlarsson8@gmail.com>
2023-04-14 15:55:25 +0200

    Data: add antd table
    This throws errors locally. Does CF also hate it?

--- app/components/Data/LastSentList.tsx
+++ app/components/Data/LastSentList.tsx
@@ -1,35 +1,39 @@
 "use client";
 
-import { useState, useTransition } from "react";
 import humanizeDuration from "humanize-duration";
+import { Table } from "antd";
 import { Outputs } from "./Charts";
 
 const LastSentList = ({ outputs }: { outputs: Outputs }) => {
 
-    const [showAllOutputs, setShowAllOutputs] = useState(false);
-    const [isPending, startTransition] = useTransition();
+    const columns = [
+        {
+            title: "Game",
+            dataIndex: "game"
+        },
+        {
+            title: "Segment",
+            dataIndex: "segment"
+        },
+        {
+            title: "Language",
+            dataIndex: "language"
+        },
+        {
+            title: "Date",
+            dataIndex: "date",
+            render: (date: number) => `${new Date(date).toUTCString()} (${humanizeDuration(date - new Date().getTime(), { round: true })} ago)`
+        },
+    ];
 
     return (
-        <div className="mb-5">
-            <button
-                className="btn btn-primary mb-2"
-                disabled={isPending}
-                onClick={() => startTransition(() => setShowAllOutputs(!showAllOutputs))}
-            >
-                {showAllOutputs ? "Show last 20" : `Show all ${outputs.length.toLocaleString("en-US")} (might freeze your browser)`} {isPending && <i className="fa-solid fa-spinner fa-spin" />}
-            </button>
-            <ul className="list-group list-group-numbered">
-                {outputs
-                    .sort((a, b) => b.date - a.date)
-                    .slice(0, showAllOutputs ? -1 : 20)
-                    .map(output => (
-                        <li key={output.date} className="list-group-item">
-                            <strong>{output.game} {output.segment}</strong> - <strong>{output.language}</strong> // <span suppressHydrationWarning title={new Date(output.date).toUTCString()}>{humanizeDuration(output.date - new Date().getTime(), { round: true })} ago</span>
-                        </li>
-                    ))
-                }
-            </ul>
-        </div>
+        <Table
+            columns={columns}
+            dataSource={outputs.sort((a, b) => b.date - a.date)}
+            bordered
+            scroll={{ x: true }}
+            pagination={{ hideOnSinglePage: true }}
+        />
     );
 };
 
