f27a9209186756878935dcee8c9222a97c58553a
Leon San José Larsson <leonlarsson8@gmail.com>
2023-10-08 01:15:53 +0200

    implement /data page
    Still WIP, might redo entirely.

--- app/data/page.tsx
+++ app/data/page.tsx
@@ -1,3 +1,9 @@
+import { Suspense } from "react";
+import humanizeDuration from "humanize-duration";
+import { StatsSentPerDayChartWithFilter, StatsSentPerGameChart, StatsSentPerGameTotalChart, StatsSentPerLanguageChart, StatsSentPerLanguageTotalChart, StatsSentPerSegmentChart } from "@/components/Charts";
+import { Icons } from "@/components/icons";
+import type { BaseStats, Output, CountsItem, UserSpecial, Event, SentDailyItemGames } from "@/types";
+
 const pageTitle = "Data | Battlefield Stats Discord Bot";
 const pageDescription = "Dive into the usage data for the Battlefield Stats Discord Bot.";
 
@@ -23,6 +29,234 @@ export default () => {
   return (
     <div className="container relative">
       <span className="text-3xl font-bold">Data</span>
+
+      <br />
+      <br />
+
+      <div>
+        <div>
+          <h2 className="text-2xl font-bold">Total (since May 25, 2021)</h2>
+          <Suspense fallback={<LoadingText />}>
+            <TotalStats />
+          </Suspense>
+        </div>
+
+        <hr className="my-6 border-2" />
+
+        <div>
+          <h2 className="text-2xl font-bold">Since January 1, 2023</h2>
+          <Suspense fallback={<LoadingText />}>
+            <SinceJanuary />
+          </Suspense>
+        </div>
+
+        <hr className="my-6 border-2" />
+
+        <div>
+          <h2 className="text-2xl font-bold">Last 20 stats sent</h2>
+          <Suspense fallback={<LoadingText />}>
+            <LastStatsSent />
+          </Suspense>
+        </div>
+
+        <hr className="my-6 border-2" />
+
+        <div>
+          <h2 className="text-2xl font-bold">Last 20 events</h2>
+          <Suspense fallback={<LoadingText />}>
+            <LastEvents />
+          </Suspense>
+        </div>
+      </div>
     </div>
   );
 };
+
+const TotalStats = async () => {
+  const res = await fetch("https://api.battlefieldstats.com/base", { next: { revalidate: 0 } });
+  if (!res.ok) return <ErrorFetchingText />;
+  const baseStats: BaseStats = await res.json();
+
+  return (
+    <>
+      <div className="mb-3">
+        <b>{baseStats.totalStatsSent.total.toLocaleString("en")}</b> stats sent
+      </div>
+
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        <div className="mb-3">
+          <h4 className="text-lg font-bold">Stats sent per game</h4>
+          <div className="flex flex-col gap-1 rounded border p-2">
+            {Object.entries(baseStats.totalStatsSent.games).map(game => (
+              <span key={game[0]}>
+                {game[0]}: <strong>{game[1].toLocaleString("en")}</strong> ({((game[1] / baseStats.totalStatsSent.total) * 100).toFixed(1)}%)
+                <hr className="my-1" />
+              </span>
+            ))}
+
+            <StatsSentPerGameTotalChart baseStats={baseStats} />
+          </div>
+        </div>
+
+        <div>
+          <h4 className="text-lg font-bold">Stats sent per language</h4>
+          <div className="flex flex-col gap-1 rounded border p-2">
+            {Object.entries(baseStats.totalStatsSent.languages).map(language => (
+              <span key={language[0]}>
+                {language[0]}: <strong>{language[1].toLocaleString("en-US")}</strong> ({((language[1] / baseStats.totalStatsSent.total) * 100).toFixed(1)}%)
+                <hr className="my-1" />
+              </span>
+            ))}
+
+            <StatsSentPerLanguageTotalChart baseStats={baseStats} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const SinceJanuary = async () => {
+  const [users, data, sentDaily] = (await Promise.all(
+    ["https://api.battlefieldstats.com/users/special", "https://api.battlefieldstats.com/outputs/counts", "https://api.battlefieldstats.com/outputs/daily/games"].map(url => fetch(url, { next: { revalidate: 0 } }).then(res => res.ok && res.json()))
+  )) as [UserSpecial[], CountsItem[], SentDailyItemGames[]];
+  if (!users || !data || !sentDaily) return <ErrorFetchingText />;
+  const games = data.filter(x => x.category === "game");
+  const segments = data.filter(x => x.category === "segment");
+  const languages = data.filter(x => x.category === "language");
+  const totalSent = games.reduce((a, b) => a + b.sent, 0);
+
+  return (
+    <>
+      <div className="mb-3">
+        <ul>
+          <li>
+            <b>{totalSent.toLocaleString("en")}</b> stats sent
+          </li>
+          <li>
+            <b>{users[0].total_users.toLocaleString("en")}</b> unique users
+          </li>
+        </ul>
+      </div>
+
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-4">
+        <div className="mb-3">
+          <h4 className="text-lg font-bold">Stats sent per game</h4>
+          <div className="flex flex-col gap-1 rounded border p-2">
+            {games.map(obj => (
+              <span key={obj.item}>
+                {obj.item}: <strong>{obj.sent.toLocaleString("en")}</strong> ({((obj.sent / totalSent) * 100).toFixed(1)}%)
+                <hr className="my-1" />
+              </span>
+            ))}
+
+            <StatsSentPerGameChart games={games} />
+          </div>
+        </div>
+
+        <div className="mb-3">
+          <h4 className="text-lg font-bold">Stats sent per segment</h4>
+          <div className="flex flex-col gap-1 rounded border p-2">
+            {segments.map(obj => (
+              <span key={obj.item}>
+                {obj.item}: <strong>{obj.sent.toLocaleString("en-US")}</strong> ({((obj.sent / totalSent) * 100).toFixed(1)}%)
+                <hr className="my-1" />
+              </span>
+            ))}
+
+            <StatsSentPerSegmentChart segments={segments} />
+          </div>
+        </div>
+
+        <div className="mb-3">
+          <h4 className="text-lg font-bold">Stats sent per language</h4>
+          <div className="flex flex-col gap-1 rounded border p-2">
+            {languages.map(obj => (
+              <span key={obj.item}>
+                {obj.item}: <strong>{obj.sent.toLocaleString("en-US")}</strong> ({((obj.sent / totalSent) * 100).toFixed(1)}%)
+                <hr className="my-1" />
+              </span>
+            ))}
+
+            <StatsSentPerLanguageChart languages={languages} />
+          </div>
+        </div>
+
+        <div className="mb-3">
+          <h4 className="text-lg font-bold">Top users</h4>
+          <div className="flex flex-col gap-1 rounded border p-2">
+            {users.slice(0, 20).map((user, index) => (
+              <span key={index}>
+                User was sent <strong>{user.total_stats_sent.toLocaleString("en-US")}</strong> stats
+                <hr className="my-1" />
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <h4 className="text-lg font-bold">Stats sent per day</h4>
+        <StatsSentPerDayChartWithFilter data={sentDaily} />
+      </div>
+    </>
+  );
+};
+
+const LastStatsSent = async () => {
+  const res = await fetch("https://api.battlefieldstats.com/outputs/last", { next: { revalidate: 0 } });
+  if (!res.ok) return <ErrorFetchingText />;
+  const outputs: Output[] = await res.json();
+  return (
+    <div className="flex flex-col gap-1 rounded border p-2">
+      {outputs.map(output => (
+        <span key={output.date}>
+          <b>
+            {output.game} {output.segment}
+          </b>{" "}
+          - <b>{output.language}</b> // <span title={new Date(output.date).toUTCString()}>{humanizeDuration(output.date - new Date().getTime(), { round: true })} ago</span>
+          <hr className="my-1" />
+        </span>
+      ))}
+    </div>
+  );
+};
+
+const LastEvents = async () => {
+  const res = await fetch("https://api.battlefieldstats.com/events/last", { next: { revalidate: 0 } });
+  if (!res.ok) return <ErrorFetchingText />;
+  const events: Event[] = await res.json();
+
+  const eventToIcon = (eventName: string) => {
+    switch (eventName) {
+      case "guildCreate":
+        return <Icons.arrowRight className="mb-1 inline h-5 w-5" />;
+      case "guildDelete":
+        return <Icons.arrowRight className="mb-1 inline h-5 w-5 rotate-180" />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-1 rounded border p-2">
+      {events.map(eventObj => (
+        <span key={eventObj.date}>
+          <b>
+            {eventToIcon(eventObj.event)} Bot {eventObj.event === "guildCreate" ? "joined" : "left"} a guild
+          </b>{" "}
+          // <span title={new Date(eventObj.date).toUTCString()}>{humanizeDuration(eventObj.date - new Date().getTime(), { round: true })} ago</span>
+          <hr className="my-1" />
+        </span>
+      ))}
+    </div>
+  );
+};
+
+const LoadingText = () => (
+  <span className="text-xl font-semibold">
+    <Icons.spinner className="inline animate-spin" /> Fetching data... This might take a while.
+  </span>
+);
+
+const ErrorFetchingText = () => <span className="text-lg font-semibold text-red-600 dark:text-red-500">Error fetching.</span>;
---
+++ components/Charts.tsx
@@ -0,0 +1,210 @@
+"use client";
+
+import { useState } from "react";
+import { Chart as ChartJS, ArcElement, CategoryScale, LinearScale, BarElement, Tooltip, Legend } from "chart.js";
+import { Bar, Doughnut } from "react-chartjs-2";
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
+import { Label } from "./ui/label";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
+import type { BaseStats, CountsItem, SentDailyItem, SentDailyItemGames } from "@/types";
+
+ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement);
+
+const backgroundColor = ["#f59b71", "#60ff78", "#f0e0fb", "#cfe2f3", "#655925", "#85754e", "#cd9b1d", "#301e4b", "#75758f", "#416788", "#c0c066", "#6666c0", "#333366", "#445577", "#883399", "#daccad", "#accec0", "#3c2c7d", "#ff800c", "#ad9a9d"];
+
+export const StatsSentPerGameChart = ({ games }: { games: CountsItem[] }) => {
+  return (
+    <Doughnut
+      data={{
+        labels: games.map(obj => obj.item),
+        datasets: [
+          {
+            label: " # of stats sent",
+            data: games.map(x => x.sent),
+            backgroundColor,
+            hoverOffset: 7
+          }
+        ]
+      }}
+    />
+  );
+};
+
+export const StatsSentPerSegmentChart = ({ segments }: { segments: CountsItem[] }) => {
+  return (
+    <Doughnut
+      data={{
+        labels: segments.map(obj => obj.item),
+        datasets: [
+          {
+            label: " # of stats sent",
+            data: segments.map(x => x.sent),
+            backgroundColor,
+            hoverOffset: 7
+          }
+        ]
+      }}
+    />
+  );
+};
+
+export const StatsSentPerLanguageChart = ({ languages }: { languages: CountsItem[] }) => {
+  return (
+    <Doughnut
+      data={{
+        labels: languages.map(obj => obj.item),
+        datasets: [
+          {
+            label: " # of stats sent",
+            data: languages.map(x => x.sent),
+            backgroundColor,
+            hoverOffset: 7
+          }
+        ]
+      }}
+    />
+  );
+};
+
+// Uses /outputs/daily/games
+export const StatsSentPerDayChartWithFilter = ({ data }: { data: SentDailyItemGames[] }) => {
+  const totalData = data.filter((v, i, a) => a.findIndex(v2 => v2.day === v.day) === i);
+
+  const [showAll, setShowAll] = useState(true);
+  const [selectedGame, setSelectedGame] = useState("All games");
+  const [selectedData, setSelectedData] = useState(totalData);
+
+  // On select change:
+  // If the selection is All games, show all
+  // Set selected game
+  // Set selected data:
+  // If All games, get each day.
+  // If not All games, get selected data from that game
+  const handleGameChange = (selectValue: string) => {
+    if (selectValue !== "All games") setShowAll(true);
+    setSelectedGame(selectValue);
+    setSelectedData(selectValue === "All games" ? totalData : data.filter(x => x.game === selectValue));
+  };
+
+  return (
+    <>
+      {selectedGame === "All games" && (
+        <>
+          <RadioGroup className="mb-2" defaultValue="yes">
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem id="r1" value="yes" defaultChecked onClick={() => setShowAll(true)} />
+              <Label htmlFor="r1">Since Jan 1st, 2023</Label>
+            </div>
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem id="r2" value="no" onClick={() => setShowAll(false)} />
+              <Label htmlFor="r2">Last 30 days</Label>
+            </div>
+          </RadioGroup>
+        </>
+      )}
+
+      <Select defaultValue="All games" onValueChange={e => handleGameChange(e)}>
+        <SelectTrigger className="w-[250px]">
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectItem defaultChecked value="All games">
+              All games
+            </SelectItem>
+            {["Battlefield 2042", "Battlefield V", "Battlefield 1", "Battlefield Hardline", "Battlefield 4", "Battlefield 3", "Battlefield Bad Company 2", "Battlefield 2"].map(game => (
+              <SelectItem key={game} value={game}>
+                {game}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+
+      <Bar
+        redraw={false}
+        key={selectedGame}
+        data={{
+          labels: selectedData.slice(showAll ? 0 : -30).map(x => x.day),
+          datasets: [
+            {
+              label: " # of stats sent",
+              data: selectedData.slice(showAll ? 0 : -30).map(x => (selectedGame === "All games" ? x.total_sent : x.sent)),
+              backgroundColor
+            }
+          ]
+        }}
+      />
+    </>
+  );
+};
+
+// Uses /outputs/daily
+export const StatsSentPerDayChart = ({ data }: { data: SentDailyItem[] }) => {
+  const [showAll, setShowAll] = useState(true);
+
+  return (
+    <>
+      <div className="form-check">
+        <input className="form-check-input" type="radio" name="flexRadioDefault" id="radio1" value="yes" defaultChecked onChange={e => setShowAll(e.target.value === "yes")} />
+        <label className="form-check-label user-select-none" htmlFor="radio1">
+          Since Jan 1st, 2023
+        </label>
+      </div>
+      <div className="form-check">
+        <input className="form-check-input" type="radio" name="flexRadioDefault" id="radio2" value="no" onChange={e => setShowAll(e.target.value === "yes")} />
+        <label className="form-check-label user-select-none" htmlFor="radio2">
+          Last 30 days
+        </label>
+      </div>
+      <Bar
+        data={{
+          labels: data.slice(showAll ? 0 : -30).map(x => x.day),
+          datasets: [
+            {
+              label: " # of stats sent",
+              data: data.slice(showAll ? 0 : -30).map(x => x.sent),
+              backgroundColor
+            }
+          ]
+        }}
+      />
+    </>
+  );
+};
+
+export const StatsSentPerGameTotalChart = ({ baseStats }: { baseStats: BaseStats }) => {
+  return (
+    <Doughnut
+      data={{
+        labels: Object.entries(baseStats.totalStatsSent.games).map(game => game[0]),
+        datasets: [
+          {
+            label: " # of stats sent",
+            data: Object.entries(baseStats.totalStatsSent.games).map(game => game[1]),
+            backgroundColor,
+            hoverOffset: 7
+          }
+        ]
+      }}
+    />
+  );
+};
+
+export const StatsSentPerLanguageTotalChart = ({ baseStats }: { baseStats: BaseStats }) => {
+  return (
+    <Doughnut
+      data={{
+        labels: Object.entries(baseStats.totalStatsSent.languages).map(language => language[0]),
+        datasets: [
+          {
+            label: " # of stats sent",
+            data: Object.entries(baseStats.totalStatsSent.languages).map(language => language[1]),
+            backgroundColor,
+            hoverOffset: 7
+          }
+        ]
+      }}
+    />
+  );
+};
