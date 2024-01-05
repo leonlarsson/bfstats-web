1744206defa17f8f92e500f817710f5bc9eb3d42
Leon San José Larsson <leonlarsson8@gmail.com>
2024-01-05 22:51:03 +0100

    HeaderStats: update icons

--- components/HeaderStats.tsx
+++ components/HeaderStats.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { HomeIcon, SendIcon, StarIcon, User2Icon } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { BaseStats } from "@/types";
 import { Icons } from "./icons";
@@ -20,10 +21,10 @@ export default async () => {
 
       {/* Boxes - Only visible on small or larger */}
       <div className="my-4 hidden gap-2 sm:grid sm:grid-cols-2 lg:grid-cols-4">
-        <StatsBox title="Servers" value={data?.totalGuilds.toLocaleString("en") ?? "Unknown"} />
-        <StatsBox title="Members" value={data?.totalMembers.toLocaleString("en") ?? "Unknown"} />
-        <StatsBox title="Stats Sent" value={data?.totalStatsSent.total.toLocaleString("en") ?? "Unknown"} />
-        <StatsBox title="Most Popular Game (all-time)" value={data ? Object.entries(data.totalStatsSent.games).sort((a, b) => b[1] - a[1])[0][0] : "Unknown"} />
+        <StatsBox title="Servers" value={data?.totalGuilds.toLocaleString("en") ?? "Unknown"} icon={<HomeIcon className="size-4 opacity-50" />} />
+        <StatsBox title="Members" value={data?.totalMembers.toLocaleString("en") ?? "Unknown"} icon={<User2Icon className="size-4 opacity-50" />} />
+        <StatsBox title="Stats Sent" value={data?.totalStatsSent.total.toLocaleString("en") ?? "Unknown"} icon={<SendIcon className="size-4 opacity-50" />} />
+        <StatsBox title="Most Popular Game" value={data ? Object.entries(data.totalStatsSent.games).sort((a, b) => b[1] - a[1])[0][0] : "Unknown"} icon={<StarIcon className="size-4 opacity-50 hover:text-yellow-300 hover:opacity-100" />} />
       </div>
 
       <hr />
@@ -32,7 +33,7 @@ export default async () => {
 };
 
 export const HeaderStatsSkeleton = () => (
-  <div className="container my-4">
+  <div className="my-4">
     <div className="block sm:hidden">
       <h3 className="my-2 text-center text-xl">
         In{" "}
@@ -52,21 +53,21 @@ export const HeaderStatsSkeleton = () => (
     </div>
 
     <div className="my-4 hidden gap-2 sm:grid sm:grid-cols-2 lg:grid-cols-4">
-      <StatsBox title="Servers" value={<Icons.spinner className="h-8 w-8 animate-spin" />} />
-      <StatsBox title="Members" value={<Icons.spinner className="h-8 w-8 animate-spin" />} />
-      <StatsBox title="Stats Sent" value={<Icons.spinner className="h-8 w-8 animate-spin" />} />
-      <StatsBox title="Most Popular Game (all-time)" value={<Icons.spinner className="h-8 w-8 animate-spin" />} />
+      <StatsBox title="Servers" value={<Icons.spinner className="size-8 animate-spin" />} icon={<HomeIcon className="size-4 opacity-50" />} />
+      <StatsBox title="Members" value={<Icons.spinner className="size-8 animate-spin" />} icon={<User2Icon className="size-4 opacity-50" />} />
+      <StatsBox title="Stats Sent" value={<Icons.spinner className="size-8 animate-spin" />} icon={<SendIcon className="size-4 opacity-50" />} />
+      <StatsBox title="Most Popular Game" value={<Icons.spinner className="size-8 animate-spin" />} icon={<StarIcon className="size-4 opacity-50" />} />
     </div>
 
     <hr />
   </div>
 );
 
-const StatsBox = ({ title, value }: { title: string; value: string | React.ReactNode }) => (
+const StatsBox = ({ title, value, icon }: { title: string; value: string | React.ReactNode; icon?: React.ReactNode }) => (
   <Link href="/data" className={cn("group flex flex-col gap-2 rounded border p-4 transition-colors hover:border-black dark:hover:border-white")}>
     <div className="flex justify-between">
       <span className="text-sm opacity-80">{title}</span>
-      <Icons.arrowRight className="h-4 w-4 opacity-50 transition-opacity group-hover:opacity-80" />
+      {icon}
     </div>
     <span className="text-2xl font-bold">{value}</span>
   </Link>
