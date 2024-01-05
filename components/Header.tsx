2e9288e438ee66ba777614e241de46885f2b1c32
Leon San José Larsson <leonlarsson8@gmail.com>
2024-01-06 00:00:43 +0100

    Header: update some header styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,7 +17,7 @@ export default () => {
         {/* Replaced with minimal header below md */}
         <div className="hidden items-center gap-1 md:flex">
           <Link href="/" className={cn("mr-3 flex items-center gap-3 text-lg font-semibold")}>
-            <Image src="/images/avatar.png" alt="Battlefield Stats icon" className="h-10 w-10 rounded" width={100} height={100} /> <span className={cn("opacity-75 transition-opacity", pathname === "/" && "opacity-100")}>Battlefield Stats</span>
+            <Image src="/images/avatar.png" alt="Battlefield Stats icon" className="size-10 rounded" width={100} height={100} /> <span className={cn("opacity-75 transition-opacity", pathname === "/" && "opacity-100")}>Battlefield Stats</span>
           </Link>
 
           <Link href="/privacy" className={cn("rounded p-2 opacity-75 transition-all hover:bg-neutral-200 dark:hover:bg-neutral-800", pathname === "/privacy" && "opacity-100")}>
@@ -34,18 +34,18 @@ export default () => {
 
           {/* First shown at lg */}
           <Link href="https://discord.com/oauth2/authorize?client_id=842768680252997662&scope=bot%20applications.commands" target="_blank" className={cn("hidden items-center gap-1 rounded p-2 transition-colors hover:bg-neutral-200 dark:hover:bg-neutral-800 lg:flex")}>
-            <Icons.discord className="h-6 w-6" /> Invite to Discord
+            <Icons.discord className="size-6" /> Invite to Discord
           </Link>
         </div>
 
         {/* Replaced with minimal header below md */}
         <div className="hidden items-center gap-1 md:flex">
-          <Link href="https://x.com/MozzyFX" target="_blank" title="X / Twitter" className="rounded p-2 transition-colors hover:bg-neutral-200 dark:hover:bg-neutral-800">
-            <Icons.twitterX className="h-6 w-6" />
+          <Link href="https://x.com/MozzyFX" target="_blank" title="X / Twitter" className="rounded p-2 opacity-80 transition-colors hover:bg-neutral-200 dark:hover:bg-neutral-800">
+            <Icons.twitterX className="size-6" />
           </Link>
 
-          <Link href="https://github.com/leonlarsson/bfstats-web" target="_blank" title="GitHub" className="rounded p-2 transition-colors hover:bg-neutral-200 dark:hover:bg-neutral-800">
-            <Icons.gitHub className="h-6 w-6" />
+          <Link href="https://github.com/leonlarsson/bfstats-web" target="_blank" title="GitHub" className="rounded p-2 opacity-80 transition-colors hover:bg-neutral-200 dark:hover:bg-neutral-800">
+            <Icons.gitHub className="size-6" />
           </Link>
 
           <ThemeModeToggle />
@@ -54,7 +54,7 @@ export default () => {
         {/* Minimal header with Sheet. Shown at md */}
         <div className="flex w-full items-center justify-between md:hidden">
           <Link href="/" className={cn("mr-3 flex items-center gap-3 text-lg font-semibold")}>
-            <Image src="/images/avatar.png" alt="Battlefield Stats icon" className="hidden h-10 w-10 rounded min-[330px]:inline" width={100} height={100} /> <span className={cn("opacity-75 transition-opacity", pathname === "/" && "opacity-100")}>Battlefield Stats</span>
+            <Image src="/images/avatar.png" alt="Battlefield Stats icon" className="size-10 hidden rounded min-[330px]:inline" width={100} height={100} /> <span className={cn("opacity-75 transition-opacity", pathname === "/" && "opacity-100")}>Battlefield Stats</span>
           </Link>
 
           <HeaderSheet />
--- components/HeaderSheet.tsx
+++ components/HeaderSheet.tsx
@@ -10,14 +10,14 @@ export default () => {
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button variant="outline">
-          <Icons.bars className="h-5 w-5" />
+        <Button variant="outline" size="icon">
+          <Icons.bars className="size-5" />
         </Button>
       </SheetTrigger>
       <SheetContent>
         <SheetHeader>
           <SheetTitle className="flex items-center gap-3">
-            <Image src="/images/avatar.png" alt="Battlefield Stats icon" className="h-10 w-10 rounded" width={100} height={100} />
+            <Image src="/images/avatar.png" alt="Battlefield Stats icon" className="size-10 rounded" width={100} height={100} />
             <span>Battlefield Stats</span>
           </SheetTitle>
         </SheetHeader>
@@ -48,18 +48,18 @@ export default () => {
           </SheetClose>
 
           <Link href="https://discord.com/oauth2/authorize?client_id=842768680252997662&scope=bot%20applications.commands" target="_blank" className={cn("flex items-center gap-1 rounded p-2 transition-colors hover:bg-neutral-200 dark:hover:bg-neutral-800")}>
-            <Icons.discord className="h-6 w-6" /> Invite to Discord
+            <Icons.discord className="size-6" /> Invite to Discord
           </Link>
         </div>
 
         <div className="flex flex-col gap-2">
           <div className="flex gap-1">
-            <Link href="https://x.com/MozzyFX" target="_blank" title="X / Twitter" className="rounded p-2 transition-colors hover:bg-neutral-200 dark:hover:bg-neutral-800">
-              <Icons.twitterX className="h-6 w-6" />
+            <Link href="https://x.com/MozzyFX" target="_blank" title="X / Twitter" className="rounded p-2 opacity-80 transition-colors hover:bg-neutral-200 dark:hover:bg-neutral-800">
+              <Icons.twitterX className="size-6" />
             </Link>
 
-            <Link href="https://github.com/leonlarsson/bfstats-web" target="_blank" title="GitHub" className="rounded p-2 transition-colors hover:bg-neutral-200 dark:hover:bg-neutral-800">
-              <Icons.gitHub className="h-6 w-6" />
+            <Link href="https://github.com/leonlarsson/bfstats-web" target="_blank" title="GitHub" className="rounded p-2 opacity-80 transition-colors hover:bg-neutral-200 dark:hover:bg-neutral-800">
+              <Icons.gitHub className="size-6" />
             </Link>
           </div>
 
