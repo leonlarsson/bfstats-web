c31fbe2025cb277e8262bab462591b0fa53899fc
Leon San José Larsson <54620188+leonlarsson@users.noreply.github.com>
2026-03-20 21:25:01 +0100

    update dev tools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -9,7 +9,7 @@ const TanStackRouterDevtools =
     ? () => null // Render nothing in production
     : lazy(() =>
         // Lazy load in development
-        import("@tanstack/router-devtools").then((res) => ({
+        import("@tanstack/react-router-devtools").then((res) => ({
           default: res.TanStackRouterDevtools,
         })),
       );
