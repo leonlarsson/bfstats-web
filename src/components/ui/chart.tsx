4a432423f3501e7102d9fbc40246b899e5e0e48c
Leon San José Larsson <54620188+leonlarsson@users.noreply.github.com>
2026-07-17 11:14:01 +0200

    en formatting

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -215,7 +215,7 @@ const ChartTooltipContent = React.forwardRef<
                       </div>
                       {item.value && (
                         <span className="font-mono font-medium tabular-nums text-foreground">
-                          {item.value.toLocaleString()}
+                          {item.value.toLocaleString("en")}
                         </span>
                       )}
                     </div>
